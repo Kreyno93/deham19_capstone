7ec63ffb9e8ac225b58cc3d11b68df8fa8756425
Omid-Mohmand <omid.mohmand1@gmail.com>
2024-10-15 14:43:31 +0200

    The Script has been editied. Now it creates ONE vpc.

--- vpc.tf
+++ vpc.tf
@@ -6,12 +6,3 @@ resource "aws_vpc" "vpc1" {
     Name = "vpc1"
   }
 }
-
-# Create a VPC for in us-west-2b
-resource "aws_vpc" "vpc2" {
-  cidr_block = "10.1.0.0/16"
-  instance_tenancy = "default"
-  tags = {
-    Name = "vpc2"
-  }
-}
